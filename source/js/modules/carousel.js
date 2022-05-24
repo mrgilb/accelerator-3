3baaa4f5b0d4b224887e8f1d24eeb54045985f40
mrgilb <ilya.nikolenko.job@gmail.com>
2022-05-25 00:42:40 +0300

    fix after test

--- source/js/modules/carousel.js
+++ source/js/modules/carousel.js
@@ -1,30 +1,40 @@
 if (document.querySelector('.accordion') && document.querySelector('.accordion__list') && document.querySelector('.accordion__item') && document.querySelector('.accordion__next') && document.querySelector('.accordion__back') && document.querySelector('.accordion__wrapper') && document.querySelector('.accordion__controls ')) {
   const carousel = document.querySelector('.accordion');
   const items = document.querySelector('.accordion__list');
-  const item = document.querySelector('.accordion__item');
   const nextButton = document.querySelector('.accordion__next');
   const prevButton = document.querySelector('.accordion__back');
   const accordionWrapper = document.querySelector('.accordion__wrapper');
   const accordionControls = document.querySelector('.accordion__controls ');
+  const allSlides = document.querySelectorAll('.accordion__item');
 
-  const allSlidesCount = document.querySelectorAll('.accordion__item').length;
-  const visibleSlides = 1;
+  const maxSlides = allSlides.length;
+  let visibleSlidesCount = 0;
+  let renderSlides = [];
+
+  prevButton.setAttribute('disabled', 'disabled');
+
+  allSlides.forEach((slide) => {
+    renderSlides.push(slide);
+  });
+
+  allSlides.forEach((slide) => {
+    slide.remove();
+  });
+
+  renderSlides.map((slide) => {
+    items.appendChild(slide);
+  });
+
 
   accordionWrapper.classList.remove('accordion__wrapper--no-js');
   items.classList.remove('accordion__list--no-js');
   accordionControls.classList.remove('accordion__controls--no-js');
 
-  let offsetSlide = parseInt(window.getComputedStyle(items, null).getPropertyValue('left'), 10);
-  if (offsetSlide <= 0) {
-    prevButton.setAttribute('disabled', 'disabled');
-  }
 
   let touchPositionStartX = null;
   let touchPositionStartY = null;
 
   const moveItems = (evt) => {
-    const stepOffset = parseInt(item.offsetWidth, 10) + parseInt(window.getComputedStyle(item).getPropertyValue('margin-right'), 10);
-    const maxOffset = ((stepOffset * allSlidesCount) - (stepOffset * visibleSlides)) * -1;
 
 
     if (evt.target.matches('.accordion__next') || evt.target.matches('.accordion__back') || evt.touches) {
@@ -39,32 +49,56 @@ if (document.querySelector('.accordion') && document.querySelector('.accordion__
           diffPositionX = touchPositionEndX - touchPositionStartX;
         }
       }
-      if (offsetSlide > maxOffset) {
-        if (evt.target.matches('.accordion__next') || diffPositionX < 0) {
-          offsetSlide = offsetSlide - stepOffset;
-          items.style.left = `${offsetSlide}px`;
+
+      if (evt.target.matches('.accordion__next') || diffPositionX < 0) {
+        if (visibleSlidesCount < maxSlides - 1) {
+          visibleSlidesCount = visibleSlidesCount + 1;
+        }
+
+        if (visibleSlidesCount === maxSlides - 1) {
+          nextButton.setAttribute('disabled', 'disabled');
+
+        }
+
+        if (visibleSlidesCount > 0) {
+          prevButton.removeAttribute('disabled');
+        }
+
+
+        for (let index = 0; index < renderSlides.length; index++) {
+          const element = renderSlides[index];
+          if (renderSlides.indexOf(element) === visibleSlidesCount) {
+            renderSlides.map((slide)=> {
+              slide.classList.add('accordion__item--display-none');
+              element.classList.remove('accordion__item--display-none');
+            });
+          }
         }
       }
 
-      if (offsetSlide <= stepOffset * -1) {
-        if (evt.target.matches('.accordion__back') || diffPositionX > 0) {
-          offsetSlide = offsetSlide + stepOffset;
-          items.style.left = `${offsetSlide}px`;
+      if (evt.target.matches('.accordion__back') || diffPositionX > 0) {
+        if (visibleSlidesCount >= 0) {
+          visibleSlidesCount = visibleSlidesCount - 1;
+        }
+
+        if (visibleSlidesCount === 0) {
+          prevButton.setAttribute('disabled', 'disabled');
+        }
+
+        if (visibleSlidesCount !== maxSlides - 1) {
+          nextButton.removeAttribute('disabled');
+        }
+
+        for (let index = 0; index < renderSlides.length; index++) {
+          const element = renderSlides[index];
+          if (renderSlides.indexOf(element) === visibleSlidesCount) {
+            renderSlides.map((slide)=> {
+              slide.classList.add('accordion__item--display-none');
+              element.classList.remove('accordion__item--display-none');
+            });
+          }
         }
       }
-
-      if (offsetSlide < 0) {
-        prevButton.removeAttribute('disabled');
-      } else if (offsetSlide <= 0) {
-        prevButton.setAttribute('disabled', 'disabled');
-      }
-
-      if (offsetSlide === maxOffset) {
-        nextButton.setAttribute('disabled', 'disabled');
-      } else {
-        nextButton.removeAttribute('disabled');
-      }
-
     }
 
   };
